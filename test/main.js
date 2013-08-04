e33a5d0e4bce5e00632a50df480312825b25a512
Eric Schoffstall <contra@wearefractal.com>
2013-08-04 01:59:22 -0700

    fix tests

--- test/main.js
+++ test/main.js
@@ -3,8 +3,8 @@ var should = require('should');
 require('mocha');
 
 describe('999', function() {
-  describe('FUNCTIONNAME()', function() {
-    it('should TASKNAME', function(done) {
+  describe('module.exports', function() {
+    it('should be right', function(done) {
       nineninenine.should.eql([9,9,9]);
       done();
     });
